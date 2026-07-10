7d7d586bfb8e81c516599d009544058d5c4978a6
Bernhard Bablok <bablokb@gmx.de>
2026-07-10 15:52:24 +0200

    added setting-example for the Sharp Memory Display

---
+++ contrib/settings_sharp400.py
@@ -0,0 +1,62 @@
+# ----------------------------------------------------------------------------
+# settings_sharp400.py: Settings for a Sharp Memory Display.
+#
+# Merge with your settings.py or use:
+#
+#    from settings_sharp400 import hw_config
+#
+# Website: https://github.com/bablokb/tesserae-device-circuitpython
+# ----------------------------------------------------------------------------
+
+import atexit
+import board
+import busio
+import displayio
+import framebufferio
+import sharpdisplay
+
+# --- basic display configuration   ------------------------------------------
+
+WIDTH      = 400
+HEIGHT     = 240
+
+# --- hardware-pins   --------------------------------------------------------
+
+SCK_PIN   = board.GP10
+MOSI_PIN  = board.GP11
+CS_PIN    = board.GP9
+LED_PIN   = board.LED
+
+# --- atexit processing   ----------------------------------------------------
+
+def at_exit(spi):
+  """ release spi """
+  spi.deinit()
+
+# --- display-factory method   ------------------------------------------------
+
+def _get_display(hal):
+  """ create display for a Sharp Memory Display """
+
+  displayio.release_displays()
+  spi = busio.SPI(SCK_PIN,MOSI=MOSI_PIN)
+  atexit.register(at_exit,spi)
+
+  # For the 400x240 display (can only be operated at 2MHz)
+  framebuffer = sharpdisplay.SharpMemoryFramebuffer(bus,PIN_CS,WIDTH,HEIGHT)
+  return framebufferio.FramebufferDisplay(framebuffer)
+
+# hardware configuration   ---------------------------------------------------
+
+class Settings:
+  pass
+
+hw_config     = Settings()
+hw_config.LED = LED_PIN
+hw_config.DISPLAY = _get_display
+
+# all optional!
+hw_config.led_blink_init = 0.1
+hw_config.led_blink_power_off = 0.1
+hw_config.led_blink_data = 0.0
+hw_config.led_blink_exception = 0.6
